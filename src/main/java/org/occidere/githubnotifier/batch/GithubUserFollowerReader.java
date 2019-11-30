5f0a0e0671d29e3cbf56c5b7371f841627e29849
occidere <occidere@naver.com>
2019-11-30 20:17:17 +0900

    테스트 데이터 수정

--- src/main/java/org/occidere/githubnotifier/batch/GithubUserFollowerReader.java
+++ src/main/java/org/occidere/githubnotifier/batch/GithubUserFollowerReader.java
@@ -2,6 +2,8 @@ package org.occidere.githubnotifier.batch;
 
 import java.util.Iterator;
 import java.util.List;
+import org.occidere.githubnotifier.api.GithubApi;
+import org.occidere.githubnotifier.api.GithubFollowerApi;
 import org.occidere.githubnotifier.vo.GithubUser;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
@@ -18,11 +20,9 @@ public class GithubUserFollowerReader implements ItemReader<GithubUser>, StepExe
 
     @Override
     public void beforeStep(StepExecution stepExecution) {
-        iterator = List.of(
-                GithubUser.builder().login("id1").build(),
-                GithubUser.builder().login("id2").build(),
-                GithubUser.builder().login("id3").build()
-        ).iterator();
+        GithubUser user1 = new GithubUser();
+        user1.setLogin("occidere");
+        iterator = List.of(user1).iterator();
     }
 
     @Override
@@ -32,6 +32,7 @@ public class GithubUserFollowerReader implements ItemReader<GithubUser>, StepExe
 
     @Override
     public GithubUser read() throws Exception {
+        GithubApi githubApi = new GithubFollowerApi();
 
         return iterator.hasNext() ? iterator.next() : null;
     }
