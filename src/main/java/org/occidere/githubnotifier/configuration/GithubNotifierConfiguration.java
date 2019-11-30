51ed5426182337ed27da634f551097d772225f2d
occidere <occidere@naver.com>
2019-11-30 20:17:41 +0900

    RestTemplate connection pool 설정

--- src/main/java/org/occidere/githubnotifier/configuration/GithubNotifierConfiguration.java
+++ src/main/java/org/occidere/githubnotifier/configuration/GithubNotifierConfiguration.java
@@ -1,7 +1,10 @@
 package org.occidere.githubnotifier.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.occidere.githubnotifier.api.GithubApi;
+import org.occidere.githubnotifier.api.GithubFollowerApi;
 import org.occidere.githubnotifier.batch.GithubUserFollowerReader;
 import org.occidere.githubnotifier.batch.GithubUserNotificationWriter;
 import org.occidere.githubnotifier.vo.GithubUser;
@@ -18,6 +21,8 @@ import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author occidere
@@ -40,20 +45,38 @@ public class GithubNotifierConfiguration extends AbstractElasticsearchConfigurat
     }
 
     @Bean
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        httpRequestFactory.setConnectTimeout(2000);
+        httpRequestFactory.setReadTimeout(3000);
+        httpRequestFactory.setHttpClient(
+            HttpClientBuilder.create()
+                .setMaxConnTotal(100)
+                .setMaxConnPerRoute(10)
+                .build());
+        return new RestTemplate(httpRequestFactory);
+    }
+
+    @Bean
+    public GithubApi followerApi() {
+        return new GithubFollowerApi();
+    }
+
+    @Bean
     public Job githubUserFollowerNotificationJob() {
         return jobBuilderFactory.get("githubUserFollowerNotificationJob")
-                .incrementer(new RunIdIncrementer())
-                .start(githubUserFollowerNotificationStep())
-                .build();
+            .incrementer(new RunIdIncrementer())
+            .start(githubUserFollowerNotificationStep())
+            .build();
     }
 
     @Bean
     public Step githubUserFollowerNotificationStep() {
         return stepBuilderFactory.get("githubUserFollowerNotificationStep")
-                .<GithubUser, GithubUser>chunk(1)
-                .reader(githubUserFollowerReader())
-                .writer(githubUserNotificationWriter())
-                .build();
+            .<GithubUser, GithubUser>chunk(1)
+            .reader(githubUserFollowerReader())
+            .writer(githubUserNotificationWriter())
+            .build();
     }
 
     @Bean
