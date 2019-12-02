bb3139133d56b4e45139906a766eb39e5ff86257
occidere <occidere@naver.com>
2019-12-02 18:06:05 +0900

    StepScope 추가

--- src/main/java/org/occidere/githubnotifier/configuration/GithubNotifierConfiguration.java
+++ src/main/java/org/occidere/githubnotifier/configuration/GithubNotifierConfiguration.java
@@ -3,17 +3,23 @@ package org.occidere.githubnotifier.configuration;
 import lombok.RequiredArgsConstructor;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.occidere.githubnotifier.api.GithubApi;
-import org.occidere.githubnotifier.api.GithubFollowerApi;
-import org.occidere.githubnotifier.batch.GithubUserFollowerReader;
-import org.occidere.githubnotifier.batch.GithubUserNotificationWriter;
-import org.occidere.githubnotifier.vo.GithubUser;
+import org.occidere.githubnotifier.batch.GithubUserFetchReader;
+import org.occidere.githubnotifier.batch.GithubUserFollowerInfoProcessor;
+import org.occidere.githubnotifier.batch.GithubUserReflectWriter;
+import org.occidere.githubnotifier.service.GithubApiRepository;
+import org.occidere.githubnotifier.service.GithubApiService;
+import org.occidere.githubnotifier.vo.GithubUserFollowerInfo;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -26,9 +32,10 @@ import org.springframework.web.client.RestTemplate;
 
 /**
  * @author occidere
- * @since 2019-11-29
+ * @since 2019. 11. 29.
+ * Blog: https://blog.naver.com/occidere
+ * Github: https://github.com/occidere
  */
-
 @Configuration
 @EnableBatchProcessing
 @EnableElasticsearchRepositories
@@ -40,54 +47,65 @@ public class GithubNotifierConfiguration extends AbstractElasticsearchConfigurat
     private final StepBuilderFactory stepBuilderFactory;
 
     @Override
+    @StepScope
     public RestHighLevelClient elasticsearchClient() {
         return RestClients.create(ClientConfiguration.create("localhost:9200")).rest();
     }
 
     @Bean
+    @StepScope
     public RestTemplate restTemplate() {
         HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
         httpRequestFactory.setConnectTimeout(2000);
         httpRequestFactory.setReadTimeout(3000);
         httpRequestFactory.setHttpClient(
-            HttpClientBuilder.create()
-                .setMaxConnTotal(100)
-                .setMaxConnPerRoute(10)
-                .build());
+                HttpClientBuilder.create()
+                        .setMaxConnTotal(100)
+                        .setMaxConnPerRoute(10)
+                        .build());
         return new RestTemplate(httpRequestFactory);
     }
 
     @Bean
-    public GithubApi followerApi() {
-        return new GithubFollowerApi();
+    @StepScope
+    public GithubApiRepository githubApiRepository() {
+        return new GithubApiService();
     }
 
     @Bean
     public Job githubUserFollowerNotificationJob() {
         return jobBuilderFactory.get("githubUserFollowerNotificationJob")
-            .incrementer(new RunIdIncrementer())
-            .start(githubUserFollowerNotificationStep())
-            .build();
+                .incrementer(new RunIdIncrementer())
+                .start(githubUserFollowerNotificationStep())
+                .build();
     }
 
     @Bean
+    @JobScope
     public Step githubUserFollowerNotificationStep() {
         return stepBuilderFactory.get("githubUserFollowerNotificationStep")
-            .<GithubUser, GithubUser>chunk(1)
-            .reader(githubUserFollowerReader())
-            .writer(githubUserNotificationWriter())
-            .build();
+                .<String, GithubUserFollowerInfo>chunk(1)
+                .reader(getGithubUserFetchReader())
+                .processor(getGithubUserFollowerInfoProcessor())
+                .writer(getGithubUserReflectWriter())
+                .build();
     }
 
     @Bean
-    public GithubUserFollowerReader githubUserFollowerReader() {
-        return new GithubUserFollowerReader();
+    @StepScope
+    public GithubUserFetchReader getGithubUserFetchReader() {
+        return new GithubUserFetchReader();
     }
 
     @Bean
-    public GithubUserNotificationWriter githubUserNotificationWriter() {
-        return new GithubUserNotificationWriter();
+    @StepScope
+    public ItemProcessor<String, GithubUserFollowerInfo> getGithubUserFollowerInfoProcessor() {
+        return new GithubUserFollowerInfoProcessor();
     }
 
-
+    @Bean
+    @StepScope
+    public ItemWriter<GithubUserFollowerInfo> getGithubUserReflectWriter() {
+        return new GithubUserReflectWriter();
+    }
 }
