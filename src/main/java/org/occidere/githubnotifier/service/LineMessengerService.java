6b2381d10aaa4cf57bd2dd70fcd337073b238fb8
occidere <occidere@naver.com>
2020-04-14 20:24:32 +0900

    Extract line message build & send functions to services

---
+++ src/main/java/org/occidere/githubnotifier/service/LineMessengerService.java
@@ -0,0 +1,17 @@
+package org.occidere.githubnotifier.service;
+
+import org.occidere.githubnotifier.vo.GithubFollowerDiff;
+import org.occidere.githubnotifier.vo.GithubRepositoryDiff;
+
+/**
+ * @author occidere
+ * @Blog: https://occidere.blog.me
+ * @Github: https://github.com/occidere
+ * @since 2020-04-14
+ */
+public interface LineMessengerService {
+
+    void sendFollowerMessageIfExist(GithubFollowerDiff githubFollowerDiff);
+
+    void sendRepositoryMessageIfExist(GithubRepositoryDiff githubRepositoryDiff);
+}
